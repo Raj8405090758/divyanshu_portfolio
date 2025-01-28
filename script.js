f504ff733dee5c2074d3d80a042311b304aa7e5e
Raj8405090758 <raj8405090758@gmail.com>
2025-01-28 09:02:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Example: Smooth Scroll for Navigation Links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
